b95459fb8abc858188cb1c2cf97d4f8403dc0228
justavojo <justavojo@icloud.com>
2026-05-03 22:38:40 +0900

    redis timeout tightly

--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ import openpyxl
 from database import get_db, init_db
 
 # redis 접속 선언
-redis_client = redis.Redis(host='localhost', port=6379, db=0)
+redis_client = redis.Redis(host='localhost', port=6379, db=0, socket_connect_timeout=1, socket_timeout=1)
 
 BASE_DIR = Path(__file__).parent
 
